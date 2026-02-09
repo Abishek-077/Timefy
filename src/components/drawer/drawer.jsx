e70b70620d3f8372841be7fe18554d29377ab7f0
Abishek Ghimire <159706061+Abishek-077@users.noreply.github.com>
2026-02-09 10:09:31 +0545

    Redesign Timefy interface

--- src/components/drawer/drawer.jsx
+++ src/components/drawer/drawer.jsx
@@ -10,20 +10,9 @@ import Typography from '@mui/material/Typography';
 import SettingContext from '../settings/settingcontext';
 import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Button } from '@mui/material';
-
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import { useTheme } from '@emotion/react';
 
 export default function SidebarDrawer() {
-    const theme = useTheme();
     let settingcontext = useContext(SettingContext);
-    const toggle = settingcontext.toggle;
-
-    const handleToggle=()=>{
-        settingcontext.setToggle( toggle ? false :true)
-    }
  
   const items = [
     {
@@ -54,7 +43,7 @@ export default function SidebarDrawer() {
                 <Typography
                     variant="h4"
                     component="h1"
-                    sx={{ display: { xs: 'none', sm: 'flex' }, padding:"0 50px 0 0", color:'#0000FF'}}
+                    sx={{ display: { xs: 'none', sm: 'flex' }, padding:"0 50px 0 0", color:'var(--primary)'}}
                 >
                     Timefy
                 </Typography>
@@ -68,11 +57,11 @@ export default function SidebarDrawer() {
                         key={item.title}
                         disablePadding
                         sx={settingcontext.stateswitch ?
-                             {color:"var(--offwhite)",padding:"20px 0"} : {color:"black", padding:"20px 0"}}>
+                             {color:"var(--offwhite)",padding:"20px 0"} : {color:"var(--text)", padding:"20px 0"}}>
 
                         
                             <ListItemIcon sx={settingcontext.stateswitch ?
-                             {color:"var(--offwhite)",padding:"10px 0",justifyContent:"flex-end"} : {color:"black", padding:"10px 0", justifyContent:"flex-end"}}>
+                             {color:"var(--offwhite)",padding:"10px 0",justifyContent:"flex-end"} : {color:"var(--text)", padding:"10px 0", justifyContent:"flex-end"}}>
                                 {item.icon}
                             </ListItemIcon>
                             <ListItemText primary={item.title}  sx={{padding:"0px 0px 0px 12px"}}/>
